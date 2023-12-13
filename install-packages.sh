7f40070491b7517705c5164beb622e2370894567
venky-mukku <venkatmukku49@gmail.com>
2023-12-13 13:06:06 +0530

    2 commit

--- install-packages.sh
+++ install-packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-USERID = $(id -u)
-if [ $USERID -nq 0 ]
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
 then
    echo "user didn't have the root access, plz provide the root access to them"
    exit 1 
