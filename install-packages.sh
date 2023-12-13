fd51faf72c6612e9843fcc5b055d72a17247fcef
venky-mukku <venkatmukku49@gmail.com>
2023-12-13 13:18:40 +0530

    2 commit

--- install-packages.sh
+++ install-packages.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 R="\e[31m"
+W="\e[0m"
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
-   echo " $R user didn't have the root access, plz provide the root access to them "
+   echo " $R user didn't have the root access, plz provide the root access to them $W"
    exit 1 
 else
    echo "user has the root access"
